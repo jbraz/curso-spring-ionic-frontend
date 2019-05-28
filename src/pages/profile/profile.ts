04ed144ab365fbfbb98cc1e61f9f2bd37ec7a664
JBraz <maxull@yahoo.com.br>
2019-05-28 16:40:27 -0300

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -35,7 +35,16 @@ export class ProfilePage {
           this.cliente = response;
           this.getImageIfExists();
         },
-        error => {});
+        error => {
+
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+
+        });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
